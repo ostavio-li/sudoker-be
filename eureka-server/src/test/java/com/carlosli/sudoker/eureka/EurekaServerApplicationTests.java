704919411c45d0da2e87ea847749ec6fd27dc938
lijiancheng <lijiancheng@yunzhangfang.com>
2021-07-14 16:30:15 +0800

    Initial commit

---
+++ eureka-server/src/test/java/com/carlosli/sudoker/eureka/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlosli.sudoker.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
