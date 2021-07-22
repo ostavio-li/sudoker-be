f22b942447908ecf59c36735fea892af68b36b14
lijiancheng <lijiancheng@yunzhangfang.com>
2021-07-22 17:53:20 +0800

    创建 Sudoku服务

---
+++ sudoku-service/src/main/java/com/carlosli/sudoker/sudoku/controller/SudokuController.java
@@ -0,0 +1,15 @@
+package com.carlosli.sudoker.sudoku.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author lijiancheng
+ * @date 2021/7/22
+ */
+
+@RestController
+@RequestMapping("/sudoku")
+public class SudokuController {
+
+}
